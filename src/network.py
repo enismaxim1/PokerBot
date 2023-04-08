03326d9e5d8f5f9e77a0024dea6f42c3a5167cd9
enismaxim1 <42425794+enismaxim1@users.noreply.github.com>
2023-04-08 16:12:50 +0200

    config

--- src/network.py
+++ src/network.py
@@ -5,7 +5,7 @@ class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.settimeout(5) 
-        self.server = '127.0.0.1'
+        self.server = '192.168.0.242'
         self.port = 5555
         self.addr = (self.server, self.port)
         self.id = self.connect()
